9861c384bfd1547d15e1a693932a90af607097aa
holtoon-bit <grishasreason@gmail.com>
2025-05-24 16:09:03 +0300

    publish library and plugin to central

--- agent/src/main/java/leafagent/utils/LogWritable.java
+++ agent/src/main/java/leafagent/utils/LogWritable.java
@@ -8,11 +8,36 @@ import java.util.Collection;
  * Interface for the class that creates the Leaf Log.
  */
 public interface LogWritable {
+    /**
+     * Create new object implementing {@link LogWritableRepository}.
+     */
     LogWritableRepository createRepository(String name);
+    /**
+     * Write new {@link BaseInfo} info.
+     * @param info - new {@link BaseInfo} info.
+     */
     void writeLeaf(BaseInfo info);
+    /**
+     * Update Leaf in the Leaf Log.
+     * @param info updated Leaf info.
+     */
     void updateLeaf(BaseInfo info);
+    /**
+     * Get the Leaf structure as {@link Collection<BaseInfo>}.
+     * @return {@link Collection<BaseInfo>}
+     */
     Collection<BaseInfo> getStruct();
+    /**
+     * Get the Leaf structure as {@link String}.
+     * @return {@link String}
+     */
     String getStringStruct();
+    /**
+     * Delete all {@link BaseInfo} from the Leaf structure.
+     */
     void clear();
+    /**
+     * Save the Leaf structure.
+     */
     void save();
 }
